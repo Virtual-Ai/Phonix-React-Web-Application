67a174ed011c08e5463ca4664c910f45dbcaf19c
Abduttayyeb M.r <55306260+Abduttayyeb@users.noreply.github.com>
2021-04-20 15:31:56 +0530

    Route to quiz

---
+++ src/pages/Quiz/Verbs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import PageTitle from '../../components/Typography/PageTitle'
+
+function Verb() {
+  return (
+    <>
+      <PageTitle>Quiz Verbs</PageTitle>
+    </>
+  )
+}
+
+export default Verb;
