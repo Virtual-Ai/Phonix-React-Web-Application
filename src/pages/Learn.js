bdace4b72e1dbdfba39b5eeb537ef8c1aa2043f9
abduttayyeb <absking786@gmail.com>
2021-05-29 21:17:08 +0530

    ModalsInLearn+Landing

--- src/pages/Learn.js
+++ src/pages/Learn.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from '@windmill/react-ui'
 import { ModalsIcon, EditIcon } from '../icons'
+import { Modal, ModalHeader, ModalBody, ModalFooter} from '@windmill/react-ui'
 
 import Image2 from '../assets/img/learn/girlr.png'
 import Image3 from '../assets/img/learn/girl22.png'
@@ -22,25 +23,70 @@ import { Card, CardBody } from '@windmill/react-ui'
 
 
 function Learn() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  function openModal() {
+    setIsModalOpen(true)
+  }
+
+  function closeModal() {
+    setIsModalOpen(false)
+  }
+
+
+
   return (
     <>
       <PageTitle>Learn</PageTitle>
 
       <div className="grid gap-6 mb-8 md:grid-cols-3">
-      	<Link to="/app/learn/basic-grammar">
+
 	      <Card colored className="mb-8 shadow-md" style={{backgroundColor:"#f4bb14" , position:"relative"}} >
 	        <CardBody>
 	        	<img src={Image2} alt="Image" style={{position:"absolute",height:"100%",right:"0"}}/>
 	          	<h4 className="text-3xl font-balsamiq text-white mt-8 font-bold"> Basic Grammar </h4>
 	         	<h4 className="text-xl font-balsamiq pb-4"> Let's Learn With</h4>	  
 
-	         	<Button iconRight={ModalsIcon} tag={Link} to="learn/basic-grammar" size="large" className="bg-white text-blue-50" > 
+	         	<Button iconRight={ModalsIcon} onClick={openModal} size="large" className="bg-white text-blue-50" > 
 	         			Start 
 	         	</Button>
 
 	        </CardBody>
 	      </Card>
-	     </Link>
+
+
+
+		<Modal isOpen={isModalOpen} onClose={closeModal}>
+
+	        <ModalHeader className='text-2xl font-balsamiq'> Quick Revision </ModalHeader>
+
+		        <ModalBody>
+		        	<h4 className='text-xl font-balsamiq'> Nouns </h4> 
+		        </ModalBody>
+
+		        <ModalFooter>
+		          <div className="hidden sm:block">
+		            <Button layout="outline" onClick={closeModal}>
+		              Cancel
+		            </Button>
+		          </div>
+		          <div className="hidden sm:block">
+		            <Button>Next</Button>
+		          </div>
+		          <div className="block w-full sm:hidden">
+		            <Button block size="large" layout="outline" onClick={closeModal}>
+		              Cancel
+		            </Button>
+		          </div>
+		          <div className="block w-full sm:hidden">
+		            <Button block size="large">
+		              Accept
+		            </Button>
+		          </div>
+		        </ModalFooter>
+
+      	</Modal>	 
+
 
 
 	      <Card colored className="mb-8 shadow-md" style={{backgroundColor:"#89cff0" , position:"relative"}}>
@@ -69,6 +115,7 @@ function Learn() {
 	      </Card>	    
 
 	  </div>
+
 
     </>
   )
