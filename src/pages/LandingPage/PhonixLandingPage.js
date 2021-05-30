3b6ebc436a64a7b36255fdedcb80b638a2fd1026
Sreenidhi Bendre <67953001+sreenidhi3@users.noreply.github.com>
2021-05-30 18:22:26 +0530

    update: landingPage

--- src/pages/LandingPage/PhonixLandingPage.js
+++ src/pages/LandingPage/PhonixLandingPage.js
@@ -5,26 +5,73 @@ import {PhonixNavBar} from './PhonixNavBar';
 import './PhonixLandingPage.css';
 
 
+
+const CardLinks=()=>{
+    return(
+        <ul>
+            <Link to="/app/play" className=" card card-g">
+	           <h3>Play</h3>
+            </Link>
+            <Link to="/app/chatbot" className="card card-p">
+    	        <h3>Chat</h3>
+            </Link>
+            <Link to="/app/learn" className="card card-v">
+                <h3>Learn</h3>   
+            </Link>
+        </ul>
+    )   
+}
+
+const MainLinks=()=>{
+    return(
+        <ul>
+           <li className="main-links"><Link to="/login"><button id="navAction" className="nav-item">Login</button></Link></li>
+            <li className="main-links"><Link to="/create-account"><button id="navAction" className="nav-item">Sign Up</button></Link></li>
+        </ul>
+    )
+    
+}
+
 const PhonixLandingPage = ()=>{
+    // const[showLinks,setShowLinks] = React.useState('false');
+    // React.useEffect(()=>{
+    //     const handleResize=()=>{
+    //         console.log('resize');
+    //     }
+    //     window.addEventListener('resize', handleResize);
+    // },[showLinks]
+    // )
+
+    // const ShowLinks =()=>{
+    //     if(window.innerWidth < 530){
+    //         return(<MainLinks/>)
+    //     }
+    //     return(<CardLinks/>)
+    // }
+    const [dimensions, setDimensions] = React.useState({ 
+        height: window.innerHeight,
+        width: window.innerWidth
+      })
+      React.useEffect(() => {
+        function handleResize() {
+          setDimensions({
+            height: window.innerHeight,
+            width: window.innerWidth
+          })
+    }
+        window.addEventListener('resize', handleResize)
+        return _ => {
+          window.removeEventListener('resize', handleResize)
+    }
+      })
     return(
         <div>
+            {console.log('hey')}
             <PhonixNavBar/>
         <section className="header-hero">
             <div className="header-links">
-                <ul> 
-                    {/* <li className="card"><h3>Play</h3></li>
-                    <li className="card"><h3>Learn</h3></li>
-                    <li className="card"><h3>Chat</h3></li> */}
-                    <Link to="/app/play" className=" card card-g">
-        	           <h3>Play</h3>
-                    </Link>
-                    <Link to="/app/chatbot" className="card card-p">
-        	           <h3>Chat</h3>
-                    </Link>
-                    <Link to="/app/learn" className="card card-v">
-                    <h3>Learn</h3>   
-                    </Link>
-                </ul>
+               
+                {window.innerWidth < 530 ? <MainLinks/> : <CardLinks/> }
             </div>
             <div className="header-brand-image animated">
                     <img src={landingpage} className="animated" alt=""></img>
