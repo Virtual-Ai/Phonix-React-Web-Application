8df4697d84f1da83098276aea5e0a176182005f9
Sneha Pujari <snehaupujari@gmail.com>
2021-06-04 11:49:29 +0530

    Update Play.js

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -5,6 +5,13 @@ import PageTitle from '../components/Typography/PageTitle'
 import SectionTitle from '../components/Typography/SectionTitle'
 import { Card, CardBody } from '@windmill/react-ui'
 import { Button } from '@windmill/react-ui'
+import './play-style.css';
+
+
+import Image2 from '../assets/img/learn/girlr.png'
+import Image3 from '../assets/img/learn/girl22.png'
+import Image4 from '../assets/img/learn/boy11.png'
+import Image5 from '../assets/img/learn/sir.png'
 
 
 function Cards() {
@@ -13,34 +20,15 @@ function Cards() {
       <PageTitle>Play</PageTitle>
 
   <SectionTitle>What to play ?</SectionTitle>
-
-      <div className="grid gap-6 mb-8 md:grid-cols-3">
-      <Link to="/app/play/verbs">
-	       <Card colored className="text-blue-50 bg-bcard" style={{backgroundImage:`url(${Image})`}}>
-	          <CardBody>   
-	            <p className="mb-4 font-semibold">VERBS</p>
-	          </CardBody>
-	        </Card>
-		</Link>        
-
-        <Link to="/app/play/nouns">
-        	<Card colored className="text-blue-50 bg-ycard">
-          		<CardBody>
-	            	<p className="mb-4 font-semibold">
-		            	NOUNS
-	    			</p>
-        	  	</CardBody>
-        	</Card>        
-        </Link>
-
-        <Card colored className="text-white bg-purple-600">
-          <CardBody>
-            <p className="mb-4 font-semibold">ADVERBS</p>
-            <p></p>
-          </CardBody>
-        </Card>
-      </div>      
-
+  <Link to = "/app/play/quiz">
+  <Card colored className="mb-8 shadow-md" style={{backgroundColor:"#89cff0" , position:"relative"}}>
+	        <CardBody>
+	        	<img src={Image3} alt="Image" className="hideWhenSmall" style={{position:"absolute",height:"100%",right:"0"}}/>
+	          	<h4 className="text-3xl font-balsamiq text-white mt-8 font-bold"> QUIZ </h4>
+	          	<h4 className="text-xl font-balsamiq"> Play with Words!</h4>
+	        </CardBody>
+	      </Card>
+  </Link>
     </>
   )
 }
